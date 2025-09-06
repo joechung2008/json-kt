5bcabfa3cec15633ec44cc35e1cd527e381bef6b
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-05 21:17:16 -0400

    Update io.spring.dependency-management to 1.1.7

--- api-springboot/build.gradle.kts
+++ api-springboot/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm")
-    id("org.springframework.boot") version "3.4.0"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
     application
 }
 
